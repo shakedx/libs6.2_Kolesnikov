0c4f95542e1ba617383bf1fa46d62eb70f840233
shakedx <kolesnikov_danil123@mail.ru>
2021-12-23 17:11:22 +0200

    Добавьте файлы проекта.

---
+++ MyLib/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class ClassLib
+    {
+        public static int SumNegative(int[,] mas)
+        {
+            int max = mas[0, 0];
+            int min = mas[0, 0];
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (mas[i, j] > max)
+                        max = mas[i, j];
+                    if (mas[i, j] < min)
+                        min = mas[i, j];
+                }
+            }
+            Console.WriteLine("Максимальное: {0}", max);
+            Console.WriteLine("Минимальное: {0}", min);
+            Console.ReadKey();
+            return max;
+
+        }
+
+    }
+}
---
+++ libs6.2_Kolesnikov/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace libs6._2_Kolesnikov
+{
+    class Program
+    {
+        static void Main()
+        {
+            //5.2 - базовый
+            int[,] mas = new int[7, 5];
+            Random rnd = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    mas[i, j] = rnd.Next(-10, 11);
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int res = MyLib.ClassLib.SumNegative(mas);
+
+        }
+    }
+}
